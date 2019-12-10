0822e6632f3292523acb3477c43a7f82f6fc84bb
Loki2236 <28462447+loki2236@users.noreply.github.com>
2019-12-10 16:36:09 -0300

    Added Mongo + Forms (Lacks Logic)

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
 
-// deberia cargar aca MongoDB
-// Cargar los documentos
-// y pasarlos en una variable al template.
+// Mongo Config
+const url = 'mongodb://localhost:27017';
+const dbName = 'talentoDigital';
+const client = new MongoClient(url);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Novedades de la empresa' });
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  console.log("Connectado a MongoDB");
+  const db = client.db(dbName);
+  const collection = db.collection('novedades');
+  // deberia cargar aca MongoDB
+  // Cargar los documentos
+  // y pasarlos en una variable al template.
+
+  collection.find({}).toArray(function(err, docs) {
+    if (!err){
+      console.log(`Se han cargado ${docs.length} documentos`);
+      /* Pongo el GET aca. */
+      router.get('/', function(req, res, next) {
+        res.render('index', { title: 'Novedades de la empresa', novedades: docs });
+      });
+
+    }
+  });
+
+
 });
+
+
+
+
+
 
 
 module.exports = router;
